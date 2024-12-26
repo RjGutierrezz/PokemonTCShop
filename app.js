f1a72d3f88ac674f1d2406c04a88857e2b526c3e
RjGutierrezz <rbg002@uark.edu>
2024-12-26 10:29:29 -0600

    Finished payment part and now working on gallery and new arrival items

--- app.js
+++ app.js
@@ -201,3 +201,15 @@ currentProductQuantities.forEach((quantity, index) => {
         quantity.style.color = "white";
     });
 });
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", ()=> {
+    payment.style.display = "flex";
+})
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
+})
